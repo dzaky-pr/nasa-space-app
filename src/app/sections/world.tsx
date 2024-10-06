93bd8447387c4ea7557910df7f6d6eefc09f0218
Dzaky Rifai <dzaky.pr.mulyono97@Gmail.com>
2024-10-06 13:46:31 +0700

    fix: biome check

--- src/app/sections/world.tsx
+++ src/app/sections/world.tsx
@@ -1,9 +1,9 @@
-import ButtonLink from '@/components/links/ButtonLink';
-import { NumberTicker } from '@/components/text/NumberTicker';
-import dynamic from 'next/dynamic';
-import * as React from 'react';
+import ButtonLink from "@/components/links/ButtonLink";
+import { NumberTicker } from "@/components/text/NumberTicker";
+import dynamic from "next/dynamic";
+import * as React from "react";
 
-const GlobeGl = dynamic(() => import('./GlobeGl'), { ssr: false });
+const GlobeGl = dynamic(() => import("./GlobeGl"), { ssr: false });
 
 export default function World() {
   const [countries, setCountries] = React.useState({
@@ -12,18 +12,21 @@ export default function World() {
   });
 
   React.useEffect(() => {
-    fetch('data/countries_with_waste.json')
+    fetch("data/countries_with_waste.json")
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
   return (
-    <section id="mission my-8" className="mx-auto rounded-lg max-w-screen-xl border-[0.5px] border-gray-700 p-4">
+    <section
+      id="mission my-8"
+      className="mx-auto rounded-lg max-w-screen-xl border-[0.5px] border-gray-700 p-4"
+    >
       <GlobeGl />
       <div className="pb-24 flex flex-col gap-8 items-center">
         <div className="text-center space-y-4 px-6">
@@ -31,7 +34,9 @@ export default function World() {
             <NumberTicker value={countries.total_waste} />
           </p>
 
-          <p className="text-gray-300 text-xl md:text-3xl font-semibold">metric tons plastic wastes around the world</p>
+          <p className="text-gray-300 text-xl md:text-3xl font-semibold">
+            metric tons plastic wastes around the world
+          </p>
         </div>
 
         <ButtonLink href="https://forms.gle/pxdyea2NgSgLVtK3A" openNewTab>
