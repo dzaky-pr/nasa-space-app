e304c27d99e7009588aa1148c791d2e7f469119e
vronnn <zhafrandzaky@gmail.com>
2024-10-06 20:06:39 +0700

    test: large files

--- src/app/sections/main-build.tsx
+++ src/app/sections/main-build.tsx
@@ -3,7 +3,7 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export function MainBuild() {
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: "build/build-1.loader.js",
-    dataUrl: "https://nasa-space-app-brown.vercel.app/build/build-1.data",
+    dataUrl: "build/build-1.data",
     frameworkUrl: "build/build-1.framework.js",
     codeUrl: "build/build-1.wasm",
   });
