78cc9b0420fac252684a5d18d23ef518814b0a2e
Dzaky Rifai <dzaky.pr.mulyono97@Gmail.com>
2024-10-06 13:45:45 +0700

    feat: update button moonface, g form, and gps tracker

--- src/app/sections/moonface.tsx
+++ src/app/sections/moonface.tsx
@@ -1,25 +1,18 @@
-import { BentoCard, BentoGrid } from "@/components/BentoGrid";
-import { Bot } from "lucide-react";
+import { BentoCard, BentoGrid } from '@/components/BentoGrid';
+import ButtonLink from '@/components/links/ButtonLink';
+import { Bot } from 'lucide-react';
 
 const features = [
   {
     Icon: Bot,
-    name: "Machine Learning Preview",
-    description:
-      "Integrating machine learning to astrophysics, we are working to modeling datasets.",
-    href: "",
-    cta: "",
-    className: "",
+    name: 'Machine Learning Preview',
+    description: 'Integrating machine learning to astrophysics, we are working to modeling datasets.',
+    href: '',
+    cta: '',
+    className: '',
     background: (
       <div className="absolute h-full w-full [mask-image:linear-gradient(to_top,transparent_5%,#000_100%)] ">
-        <video
-          src="https://res.cloudinary.com/dzaky/video/upload/v1728183847/testing-machine-learning_xmnubn.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="pointer-events-none h-full w-full object-cover  group-hover:scale-105"
-        />
+        <video src="https://res.cloudinary.com/dzaky/video/upload/v1728183847/testing-machine-learning_xmnubn.mp4" autoPlay loop muted playsInline className="pointer-events-none h-full w-full object-cover  group-hover:scale-105" />
       </div>
     ),
   },
@@ -27,10 +20,7 @@ const features = [
 
 export default function Moonface() {
   return (
-    <section
-      id="moonface"
-      className="my-16 mx-auto rounded-lg p-4 max-w-screen-xl border-[0.5px] border-gray-700"
-    >
+    <section id="moonface" className="my-16 mx-auto rounded-lg p-4 max-w-screen-xl border-[0.5px] border-gray-700">
       <div className="gap-8 grid grid-cols-2">
         <BentoGrid>
           {features.map((feature, idx) => (
@@ -38,18 +28,14 @@ export default function Moonface() {
           ))}
         </BentoGrid>
         <div className="flex flex-col gap-2">
-          <h1 className="text-center text-sm font-regular text-white md:text-lg pb-4">
-            Neutrack Moon Face Prediction Model
-          </h1>
+          <h1 className="text-center text-sm font-regular text-white md:text-lg pb-4">Neutrack Moon Face Prediction Model</h1>
           <p className="text-white text-justify py-8 ">
-            This innovative computer vision model brings the moon's phases
-            closer to everyone, especially those who cannot see them. Inspired
-            by NeuTrack, a device designed to help the visually impaired connect
-            with the universe, this model detects and classifies the moon's
-            various phases. By embedding this AI technology, users can
-            effortlessly discover the current moon phase without relying on
-            sight, offering a new way to experience the night sky.
+            This innovative computer vision model brings the moon's phases closer to everyone, especially those who cannot see them. Inspired by NeuTrack, a device designed to help the visually impaired connect with the universe, this model
+            detects and classifies the moon's various phases. By embedding this AI technology, users can effortlessly discover the current moon phase without relying on sight, offering a new way to experience the night sky.
           </p>
+          <ButtonLink href="https://huggingface.co/spaces/SteelAwsm/testgrad" openNewTab>
+            Let's try the machine!
+          </ButtonLink>
         </div>
       </div>
     </section>
--- src/app/sections/world.tsx
+++ src/app/sections/world.tsx
@@ -1,9 +1,9 @@
-import ButtonLink from "@/components/links/ButtonLink";
-import { NumberTicker } from "@/components/text/NumberTicker";
-import dynamic from "next/dynamic";
-import * as React from "react";
+import ButtonLink from '@/components/links/ButtonLink';
+import { NumberTicker } from '@/components/text/NumberTicker';
+import dynamic from 'next/dynamic';
+import * as React from 'react';
 
-const GlobeGl = dynamic(() => import("./GlobeGl"), { ssr: false });
+const GlobeGl = dynamic(() => import('./GlobeGl'), { ssr: false });
 
 export default function World() {
   const [countries, setCountries] = React.useState({
@@ -12,21 +12,18 @@ export default function World() {
   });
 
   React.useEffect(() => {
-    fetch("data/countries_with_waste.json")
+    fetch('data/countries_with_waste.json')
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       });
   }, []);
 
   return (
-    <section
-      id="mission my-8"
-      className="mx-auto rounded-lg max-w-screen-xl border-[0.5px] border-gray-700 p-4"
-    >
+    <section id="mission my-8" className="mx-auto rounded-lg max-w-screen-xl border-[0.5px] border-gray-700 p-4">
       <GlobeGl />
       <div className="pb-24 flex flex-col gap-8 items-center">
         <div className="text-center space-y-4 px-6">
@@ -34,12 +31,12 @@ export default function World() {
             <NumberTicker value={countries.total_waste} />
           </p>
 
-          <p className="text-gray-300 text-xl md:text-3xl font-semibold">
-            metric tons plastic wastes around the world
-          </p>
+          <p className="text-gray-300 text-xl md:text-3xl font-semibold">metric tons plastic wastes around the world</p>
         </div>
 
-        <ButtonLink href="">Save the earth now</ButtonLink>
+        <ButtonLink href="https://forms.gle/pxdyea2NgSgLVtK3A" openNewTab>
+          Save the earth now!
+        </ButtonLink>
       </div>
     </section>
   );
