a2bd98cc13a30949f81e86d20c1536f61e3d396a
vronnn <zhafrandzaky@gmail.com>
2024-10-06 15:33:16 +0700

    test: smaller unity model

--- src/app/sections/main-build.tsx
+++ src/app/sections/main-build.tsx
@@ -2,10 +2,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export function MainBuild() {
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-    loaderUrl: "build/build-pre-3.loader.js",
-    dataUrl: "build/build-pre-3.data",
-    frameworkUrl: "build/build-pre-3.framework.js",
-    codeUrl: "build/build-pre-3.wasm",
+    loaderUrl: "build/build-pre-4.loader.js",
+    dataUrl: "build/build-pre-4.data",
+    frameworkUrl: "build/build-pre-4.framework.js",
+    codeUrl: "build/build-pre-4.wasm",
   });
 
   return (
