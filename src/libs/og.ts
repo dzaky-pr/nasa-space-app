860efcf51dc830049ee50aedaae9326cde6be252
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-10-07 21:52:20 +0700

    fix: og

--- src/libs/og.ts
+++ src/libs/og.ts
@@ -4,11 +4,18 @@ type OpenGraphType = {
   templateTitle?: string;
   logo?: string;
 };
-export function openGraph({ siteName, templateTitle, description, logo = 'https://neutrack-ai-cosmos.vercel.app/images/og/og-long.png' }: OpenGraphType): string {
+export function openGraph({
+  siteName,
+  templateTitle,
+  description,
+  logo = "https://neutrack-ai-cosmos.vercel.app/images/og/og-long.png",
+}: OpenGraphType): string {
   const ogLogo = encodeURIComponent(logo);
   const ogSiteName = encodeURIComponent(siteName.trim());
-  const ogTemplateTitle = templateTitle ? encodeURIComponent(templateTitle.trim()) : undefined;
+  const ogTemplateTitle = templateTitle
+    ? encodeURIComponent(templateTitle.trim())
+    : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  return `https://og.neutrack-ai-cosmos.vercel.app/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''}`;
+  return `https://og.neutrack-ai-cosmos.vercel.app/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ""}`;
 }
